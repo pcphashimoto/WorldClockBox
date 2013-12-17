c25ad81fa82a70bda93e3557adbc5506c19170fb
satoru <s_hashimoto@pcp.co.jp>
2013-12-18 02:15:49 +0900

    added box Update Request from App

--- js/box.js
+++ js/box.js
@@ -8,15 +8,7 @@ $(function() {
 					+ (locale.offset * 60 * 60 * 1000));
 			$(".locale").text(locale.name);
 			localStorage.setItem("locale", JSON.stringify(locale));
-		} else {
-			var str = localStorage.getItem("locale");
-			locale = JSON.parse(str);
-			if (locale) {
-				d = new Date(d.getTime() + (d.getTimezoneOffset() * 60 * 1000)
-						+ (locale.offset * 60 * 60 * 1000));
-				$(".locale").text(locale.name);
-			}
-		}
+		} 
 		var h = (d.getHours() + d.getMinutes() / 60) * 30 - 180;
 		var m = (d.getMinutes() + d.getSeconds() / 60) * 6 - 180;
 		var s = d.getSeconds() * 6 - 180;
@@ -35,8 +27,22 @@ $(function() {
 		}, 300);
 
 	}
+	var gInfo = getParameters();
+	if(gInfo["locale"]){
+		var locale = gInfo["locale"].split(",")
+		setDate({
+			name : locale[0],
+			offset: locale[1]
+		});
+	}else{
+		var str = localStorage.getItem("locale")
+		,locale = JSON.parse(str);
+		if(locale){
+			setDate(locale);
+		}
+		setDate();
+	}
 	
-	setDate();
 	
 	$.when(widgetReady()).done(function() {
 		window.addEventListener("pdmessage", onPdMessage);
@@ -50,6 +56,18 @@ $(function() {
 		}
 	}
 });
+function getParameters() 
+{
+	console.log(window.location.search)
+   var searchString = window.location.search.substring(1), params = searchString.split("&"), info = {};
+   for (var i = 0; i < params.length; i++) 
+   {
+      var val = params[i].split("=");
+      info[unescape(val[0])] = unescape(val[1]);
+   }
+        
+   return info;
+}
 
 function widgetReady() {
 	var d = new $.Deferred();
@@ -62,7 +80,8 @@ function widgetReady() {
 	}
 	return d.promise();
 }
-
+/*
 setTimeout(function() {
 	location.reload();
 }, 29000);
+*/
--- js/main.js
+++ js/main.js
@@ -39,7 +39,34 @@ var init = function () {
     // add eventListener for tizenhwkey (Back Button)
     document.addEventListener( 'tizenhwkey', backEvent );
     backEventListener = backEvent;
+    
+    $(".updatewidget").click(function(){
+    	//手動Update（クエリは効く）
+		var appControl = getWidgetUpdateControl("updateType=direct&locale=" + $(this).data("locale"))
+		tizen.application.launchAppControl(appControl, "dbox.web-provider");
+    });
+    
+    (function(){
+    	tizen.alarm.removeAll();
+    	//30秒ごとに自動Update登録（クエリは効かない）
+		var appControl = getWidgetUpdateControl("updateType=direct&locale=Osaka,9");//効かない
+		var alarm = new tizen.AlarmRelative(0, 29);
+	    tizen.alarm.add(alarm, "dbox.web-provider", appControl);
+		console.log(alarm.id);
+    })();
+
+    function getWidgetUpdateControl(arg){
+		var contentInfo = arg || "";
+		var appControlData = new tizen.ApplicationControlData("content-info", [contentInfo]);
+		var appControl = new tizen.ApplicationControl(
+		   "http://tizen.org/appcontrol/operation/dynamicbox/web/update",
+		   "box-service://DcW1rMjjnT.WorldClockBox.default",
+			   null, null, [appControlData]);
+		return appControl;
+    }
+    
 };
+
 
 $(document).bind( 'pageinit', init );
 $(document).unload( unregister );
